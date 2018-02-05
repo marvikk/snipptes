eff9e956172429511a8b5831e17846c799d81156
Me <russzkij@yahoo.com>
2018-02-05 20:20:40 +0200

    c

--- react-native/async_action_w_AsyncStorage.js
+++ react-native/async_action_w_AsyncStorage.js
@@ -1,14 +1,30 @@
+//AsyncStorage is the same as Local Storage. it returns a promise. example:
+//await AsyncStorage.setItem('myItem', myItem)
+//await AsyncStorage.getItem('myItem')
+
 export const asyncAction = () => async dispatch => {
-	//AsyncStorage is the same as Local Storage. example:
-	//AsyncStorage.setItem('myItem', myItem)
-	//AsyncStorage.getItem('myItem')
+	let token = await AsyncStorage.getItem("fb_token");
 
-	let item = await AsyncStorage.getItem("myItem");
-
-	if (item) {
+	if (token) {
 		//Dispatch an action
-		dispatch({ type: ITEM_RECEIVED, payload: item });
+		dispatch({ type: LOGIN_SUCCESS, payload: token });
 	} else {
-		//do smth else
+		doLogin(dispatch);
 	}
+};
+
+//this callback is not a separate action creator it is part of asynAction but dispatch still has to be passed for an async request inside of the callback function
+const doLogin = async dispatch => {
+	//facebook example
+	let { type, token } = await Facebook.logInWithReasPermissions("someUserId", {
+		permissions: ["public_profile"]
+	});
+
+	if (type === "cancel") {
+		return dispatch({ type: LOGIN_FAIL });
+	}
+
+	await AsyncStorage.setItem("fb_token", token);
+	//if setItem fails we won't despatch the action
+	dispatch({ type: LOGIN_SUCESS, payload: token });
 };
