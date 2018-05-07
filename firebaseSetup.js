9723a27fe7822d2f4ebac4ed51d5ee8cb3cf25af
marvikk <russzkij@yahoo.com>
2018-05-07 03:05:23 +0300

    Create firebaseSetup.js

---
+++ firebaseSetup.js
@@ -0,0 +1,16 @@
+/*
+go to console.firebase.google.com
+add new project..
+set up auth method email/pass
+
+npm install --save firebase
+
+in firebase console click WEB SETUP, copy config
+
+import firebase from 'firebase'
+
+componentWillMount() {
+  firebase.initializeApp({...config})
+}
+
+*/
